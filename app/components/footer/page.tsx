19d27f2dce8d64b2285ed55a61953c27ab444b92
Olsen <chrisolsen1993@gmail.com>
2023-10-14 14:48:51 -0400

    created the footer

--- app/components/footer/page.tsx
+++ app/components/footer/page.tsx
@@ -1,3 +1,8 @@
 export default function Footer() {
-  return <h1>footer</h1>;
+  const currentDate = new Date();
+  return (
+    <h1 className="fixed opacity-200 bottom-0 w-full py-2 bg-orange-300 text-xs text-black-400  text-center -z-1 max-[600px]:hidden">
+      {`Kuisine229 © ${currentDate.getFullYear()} Let's Get Connected 🔃`}
+    </h1>
+  );
 }
